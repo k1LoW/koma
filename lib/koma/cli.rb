c507bf43479e37c21fd8116a6fb1cfc8c0264240
k1LoW <k1lowxb@gmail.com>
2016-03-18 16:56:09 +0900

    Add identity_file option

--- lib/koma/cli.rb
+++ lib/koma/cli.rb
@@ -6,7 +6,16 @@ include Specinfra::Helper::Set
 module Koma
   class CLI < Thor
     desc 'ssh', 'stdout server inventory'
-    option :key, type: :string, banner: '<key1,key2,..>', desc: 'inventory keys', :aliases => :k
+    option :key,
+           type: :string,
+           banner: '<key1,key2,..>',
+           desc: 'inventory keys',
+           aliases: :k
+    option :identity_file,
+           type: :string,
+           banner: '<identity_file>',
+           desc: 'identity file',
+           aliases: :i
     def ssh(host)
       user, host = host.split('@') if host.include?('@')
       set :backend, :ssh
@@ -14,6 +23,7 @@ module Koma
       set :request_pty, true
       ssh_options = Net::SSH::Config.for(host)
       ssh_options[:user] = user if user
+      ssh_options[:keys] = [options[:identity_file]] if options[:identity_file]
       set :ssh_options, ssh_options
       puts JSON.pretty_generate out(options[:key])
     end
