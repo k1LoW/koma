cb638d3a16e9592808fea80968299df88ed09af7
k1LoW <k1lowxb@gmail.com>
2016-03-18 16:42:48 +0900

    Add key option

--- lib/koma/cli.rb
+++ lib/koma/cli.rb
@@ -6,24 +6,45 @@ include Specinfra::Helper::Set
 module Koma
   class CLI < Thor
     desc 'ssh', 'stdout server inventory'
+    option :key, type: :string, banner: '<key1,key2,..>', desc: 'inventory keys', :aliases => :k
     def ssh(host)
       user, host = host.split('@') if host.include?('@')
       set :backend, :ssh
       set :host, host
       set :request_pty, true
-      options = Net::SSH::Config.for(host)
-      options[:user] = user if user
-      set :ssh_options, options
-      puts JSON.pretty_generate out
+      ssh_options = Net::SSH::Config.for(host)
+      ssh_options[:user] = user if user
+      set :ssh_options, ssh_options
+      puts JSON.pretty_generate out(options[:key])
+    end
+
+    desc 'keys', 'server inventory keys'
+    def keys
+      inventory_keys.each do |key|
+        puts key
+      end
+    end
+
+    def method_missing(name)
+      ssh(name.to_s)
     end
 
     private
 
-    def out
+    def inventory_keys
+      Specinfra::HostInventory.inventory_keys
+    end
+
+    def out(key = nil)
       out = {}
-      Specinfra::HostInventory.inventory_keys.each do |key|
-        inventory = Specinfra.backend.host_inventory[key]
-        out[key] = inventory unless inventory.nil?
+      keys = if key.nil?
+               inventory_keys
+             else
+               key.split(',')
+             end
+      keys.each do |k|
+        inventory = Specinfra.backend.host_inventory[k]
+        out[k] = inventory unless inventory.nil?
       end
       out
     end
