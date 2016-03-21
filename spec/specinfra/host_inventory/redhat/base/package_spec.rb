cb34dcccd366eeffece821a5cdad236629f27509
k1LoW <k1lowxb@gmail.com>
2016-03-21 12:23:37 +0900

    Fix spec

--- spec/specinfra/host_inventory/redhat/base/package_spec.rb
+++ spec/specinfra/host_inventory/redhat/base/package_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 
 str = <<-EOH
 name:libndp\tversion:1.2\trelease:4.el7\tarch:x86_64\tinstalltime:1454045569\tbuildtime:1402354146
-name:filesystemversion:3.2\trelease:20.el7\tarch:x86_64\tinstalltime:1454045531\tbuildtime:1439389384
+name:filesystem\tversion:3.2\trelease:20.el7\tarch:x86_64\tinstalltime:1454045531\tbuildtime:1439389384
 name:dmidecode\tversion:2.12\trelease:9.el7\tarch:x86_64\tinstalltime:1454045569\tbuildtime:1447997958
 name:ncurses-base\tversion:5.9\trelease:13.20130511.el7arch:noarch\tinstalltime:1454045535\tbuildtime:1402456336
 name:bind-license\tversion:9.9.4\trelease:29.el7\tarch:noarch\tinstalltime:1454045569\tbuildtime:1448032358
 name:dracut-network\tversion:033\trelease:359.el7arch:x86_64\tinstalltime:1454045570\tbuildtime:1448017425
 name:bash\tversion:4.2.46\trelease:19.el7\tarch:x86_64\tinstalltime:1454045542\tbuildtime:1447995893
-name:NetworkManager-tuiversion:1.0.6\trelease:27.el7\tarch:x86_64\tinstalltime:1454045571\tbuildtime:1448038585
 name:zlib\tversion:1.2.7\trelease:15.el7\tarch:x86_64\tinstalltime:1454045542\tbuildtime:1448010635
 name:tuned\tversion:2.5.1\trelease:4.el7\tarch:noarch\tinstalltime:1454045571\tbuildtime:1448034038
 name:libuuid\tversion:2.23.2\trelease:26.el7\tarch:x86_64\tinstalltime:1454045543\tbuildtime:1448023070
@@ -24,14 +23,12 @@ describe Specinfra::HostInventory::Package do
     ret = Specinfra::HostInventory::Package.new(host_inventory).parse(str)
     example do
       expect(ret).to include(
-       'NetworkManager-tuiversion:1.0.6' => { 'name' => 'NetworkManager-tuiversion:1.0.6', 'release' => '27.el7', 'arch' => 'x86_64', 'installtime' => '1454045571', 'buildtime' => '1448038
-585' },
        'bash' => { 'name' => 'bash', 'version' => '4.2.46', 'release' => '19.el7', 'arch' => 'x86_64', 'installtime' => '1454045542', 'buildtime' => '1447995893' },
        'bind-license' => { 'name' => 'bind-license', 'version' => '9.9.4', 'release' => '29.el7', 'arch' => 'noarch', 'installtime' => '1454045569', 'buildtime' => '1448032358' },
        'chkconfig' => { 'name' => 'chkconfig', 'version' => '1.3.61', 'release' => '5.el7', 'arch' => 'x86_64', 'installtime' => '1454045543', 'buildtime' => '1447996314' },
        'dmidecode' => { 'name' => 'dmidecode', 'version' => '2.12', 'release' => '9.el7', 'arch' => 'x86_64', 'installtime' => '1454045569', 'buildtime' => '1447997958' },
        'dracut-network' => { 'name' => 'dracut-network', 'version' => '033', 'release' => '359.el7arch:x86_64', 'installtime' => '1454045570', 'buildtime' => '1448017425' },
-       'filesystemversion:3.2' => { 'name' => 'filesystemversion:3.2', 'release' => '20.el7', 'arch' => 'x86_64', 'installtime' => '1454045531', 'buildtime' => '1439389384' },
+       'filesystem' => { 'name' => 'filesystem', 'version' => '3.2', 'release' => '20.el7', 'arch' => 'x86_64', 'installtime' => '1454045531', 'buildtime' => '1439389384' },
        'firewalld' => { 'name' => 'firewalld', 'version' => '0.3.9', 'release' => '14.el7', 'arch' => 'noarch', 'installtime' => '1454045572', 'buildtime' => '1448022902' },
        'grep' => { 'name' => 'grep', 'version' => '2.20', 'release' => '2.el7', 'arch' => 'x86_64', 'installtime' => '1454045543', 'buildtime' => '1448000049' },
        'libndp' => { 'name' => 'libndp', 'version' => '1.2', 'release' => '4.el7', 'arch' => 'x86_64', 'installtime' => '1454045569', 'buildtime' => '1402354146' },
