be845d3a7d19e447ee1c97e664f714e530a53681
k1LoW <k1lowxb@gmail.com>
2016-04-03 00:52:29 +0900

    Add `port` option

--- lib/koma/cli.rb
+++ lib/koma/cli.rb
@@ -16,6 +16,11 @@ module Koma
            banner: '<identity_file>',
            desc: 'identity file',
            aliases: :i
+    option :port,
+           type: :numeric,
+           banner: '<port>',
+           desc: 'port',
+           aliases: :p
     def ssh(host)
       user, host = host.split('@') if host.include?('@')
       set :backend, :ssh
@@ -24,6 +29,7 @@ module Koma
       ssh_options = Net::SSH::Config.for(host)
       ssh_options[:user] = user if user
       ssh_options[:keys] = [options[:identity_file]] if options[:identity_file]
+      ssh_options[:port] = options[:port] if options[:port]
       set :ssh_options, ssh_options
       puts JSON.pretty_generate out(options[:key])
     end
