7635c94bb65281e206a83fbabf2451911287309b
k1LoW <k1lowxb@gmail.com>
2016-06-17 11:53:02 +0900

    Add Specinfra::HostInventory::Parser::Redhat::Base::Package

---
+++ lib/koma/ext/specinfra/host_inventory/parser/redhat/base/package.rb
@@ -0,0 +1,14 @@
+class Specinfra::HostInventory::Parser::Redhat::Base::Package < Specinfra::HostInventory::Parser::Base
+  class << self
+    def parse(cmd_ret)
+      packages = {}
+      lines = cmd_ret.split(/\n/)
+      lines.each do |line|
+        h = Hash[line.split("\t").map { |f| f.split(':', 2) }]
+        idx = h['name']
+        packages[idx] = h
+      end
+      packages
+    end
+  end
+end
